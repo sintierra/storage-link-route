23e4130f6a3da6a035c9b13edabe40774325e1e9
sintierra <marcosditrana@gmail.com>
2022-10-23 16:14:47 -0300

    Creado service provider

---
+++ src/StorageLinkRouteServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sintierra\StorageLinkRoute;
+
+use Illuminate\Support\ServiceProvider;
+
+class StorageLinkRouteServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../' . 'routes/web.php');
+    }
+}
