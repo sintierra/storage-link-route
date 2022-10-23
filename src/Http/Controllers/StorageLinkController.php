e321ed9ec61007b9fadb9373f1b26eaf11a19b0d
sintierra <marcosditrana@gmail.com>
2022-10-23 16:15:55 -0300

    Creado controller para generar el symlink de

---
+++ src/Http/Controllers/StorageLinkController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sintierra\StorageLinkRoute\Http\Controllers;
+
+use Illuminate\Filesystem\Filesystem;
+
+class StorageLinkController
+{
+    public function __invoke(Filesystem $filesystem)
+    {
+        //Verifies that storage exists in public path
+        if($filesystem->exists(public_path('storage'))) {
+            return 'The "public/storage" already exists.'; 
+        }
+
+        //Links the storage_path of the app with public_path in folder
+        $filesystem->link(
+            storage_path('app/public'), public_path('storage')
+        );
+
+        return "The [public/storage] directory has been linked";
+    }
+}
