ed16dfef019298dc5e256741b3d97e6d478ebe55
sintierra <marcosditrana@gmail.com>
2022-10-23 18:39:32 -0300

    Creados tests para probar features de controller

--- tests/Feature/StorageLinkRouteTest.php
+++ tests/Feature/StorageLinkRouteTest.php
@@ -2,6 +2,7 @@
 
 namespace Sintierra\StorageLinkRoute\Tests\Feature;
 
+use Illuminate\Filesystem\Filesystem;
 use Sintierra\StorageLinkRoute\Tests\TestCase;
 
 
@@ -11,6 +12,30 @@ class StorageLinkRouteTest extends TestCase
     function can_execute_storage_link_command_from_a_route()
     {
         $this->withExceptionHandling();
-        $this->get('storage-link')->assertSuccessful();
+
+        //Mock the filesystem class with a spy
+        $spy = $this->spy(Filesystem::class);   
+
+        //Asserts texts shows in route
+        $this->get('storage-link')->assertSeeText('The [public/storage] directory has been linked');
+
+        //link method with storage linked
+        $spy->shouldHaveReceived('link')->with(
+            storage_path('app/public'), public_path('storage')
+        );
+
+        //Exists method with public path storage
+        $spy->shouldHaveReceived('exists')->with(public_path('storage'));
+    }
+    /** @test */
+    function cannot_try_to_create_the_storage_link_twice()
+    {
+        //MOck the filesystem, exists method return true
+        $this->mock(Filesystem::class)
+             ->shouldReceive('exists')
+             ->andReturn(true);
+
+        //Asserts that see the text
+        $this->get('storage-link')->assertSeeText('The public/storage directory already exists.');
     }
 }
